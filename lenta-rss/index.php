69d2f0905a66ca6ccb6fe58ac404789ba8c77d0c
mitrex4444 <44heroh/2mail.ru>
2017-09-17 17:16:36 +0300

    'test-2'

--- lenta-rss/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$url = 'https://lenta.ru/rss';
-$news = simplexml_load_file($url, "SimpleXMLElement",LIBXML_NOCDATA);
-for($i = 5; $i > 0; $i--){
-    echo $news->channel->item[$i]->title;
-    echo $news->channel->item[$i]->link;
-    echo $news->channel->item[$i]->description;
-}
-?>
--- soba.d7dull/lang/ru/install/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
-
-$MESS['MODULE_NAME'] = 'D7 Образец';
-$MESS['MODULE_DESCRIPTION'] = 'D7 Образец';
